40a7efb65ed2708c6662d52a4bbe075ff48ccfa1
oscarsm7 <oscarsm7@gmail.com>
2015-07-15 16:52:39 -0500

    initial

--- configs/admin.py
+++ configs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import JerarquiaAcad, Institucion
+from .models import JerarquiaAcad, Institucion, Facultad, Escuela
 # Register your models here.
 
 
@@ -12,5 +12,19 @@ class InstitucionAdmin(admin.ModelAdmin):
     search_fields = ("nombre", "abrev", "codigo")
     list_per_page = 2
 
+
+class FacultadAdmin(admin.ModelAdmin):
+    list_display = ("jerarquia_acad", "nombre", "abrev", "codigo")
+    search_fields = ("nombre", "abrev", "codigo")
+    list_per_page = 2
+
+
+class EscuelaAdmin(admin.ModelAdmin):
+    list_display = ("jerarquia_acad", "nombre", "abrev", "codigo")
+    search_fields = ("nombre", "abrev", "codigo")
+    list_per_page = 2
+
 admin.site.register(JerarquiaAcad, JerarquiaAcadAdmin)
 admin.site.register(Institucion, InstitucionAdmin)
+admin.site.register(Facultad, FacultadAdmin)
+admin.site.register(Escuela, EscuelaAdmin)
